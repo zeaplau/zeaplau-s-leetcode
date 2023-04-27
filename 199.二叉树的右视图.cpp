c3b6b03f42aa9ab32d771a2c73f6271fe1934d47
AyakuraMei <1187128291@qq.com>
2023-04-27 21:44:21 +0800

    4.27

--- 199.二叉树的右视图.cpp
+++ 199.二叉树的右视图.cpp
@@ -21,23 +21,41 @@
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
 
 class Solution {
 public:
     vector<int> layer_nodes;
-    vector<TreeNode*> q;
+    queue<TreeNode*> q;
     vector<int> rightSideView(TreeNode* root) {
-        // layer
-
-        // contain the last one
+        if (root == nullptr) {
+            return {};
+        }
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (i == size - 1) {
+                    layer_nodes.push_back(node->val);
+                }
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return this->layer_nodes;
     }
 };
 // @lc code=end
--- 200.岛屿数量.cpp
+++ 200.岛屿数量.cpp
@@ -5,10 +5,58 @@
  */
 
 // @lc code=start
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
+    int res;
+    // row, col
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        int nr = grid.size();
+        int nc = grid[0].size();
+
+        // 遇到的 '1' 标记为 '0'，就是路过的陆地直接不要了
+        // 方便后面计算
+        grid[r][c] = '0';
+
+        // 如果往上走
+        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+            dfs(grid, r - 1, c);
+        }
+        // 如果往下走
+        if (r + 1 < nr && grid[r + 1][c] == '1') {
+            dfs(grid, r + 1, c);
+        }
+        // 如果往右走
+        if (c + 1 < nc && grid[r][c + 1] == '1') {
+            dfs(grid, r, c + 1);
+        }
+        // 如果往左走
+        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+            dfs(grid, r, c - 1);
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
-        
+        int nr = grid.size();
+        if (nr == 0) {
+            return 0;
+        }
+        int nc = grid[0].size();
+
+        // 扫描每一个点
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                if (grid[i][j] == '1') {
+                    ++res;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return this->res;
     }
 };
 // @lc code=end
